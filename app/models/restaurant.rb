effb179f4237da9ff22845ff0ba6ab7f9ad6858e
STEPHANIE <WU.STEPHANIE@LIVE.COM>
2017-02-08 15:11:49 -0500

    Create restaurant associations

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,2 +1,6 @@
 class Restaurant < ApplicationRecord
+  has_many :users, through: :reservations
+  has_many :reservations
+  belongs_to :category
+  # belongs_to :owner
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+Restaurant.create(name: "Kinka Izakaya", address: "123 Toronto Road", capacity: 100, phone: "416-333-4444", description: "Trendy Japanese izakaya.", website: "http://www.kinkaizakaya.com/", owner_id: 1, category_id: 1)
+Restaurant.create(name: "Bar Hop", address: "321 Toronto Road", capacity: 100, phone: "416-332-4254", description: "Largest beer selection in Toronto.", website: "http://www.barhop.com/", owner_id: 2, category_id: 2)
