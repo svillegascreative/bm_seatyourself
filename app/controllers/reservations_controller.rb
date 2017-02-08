a7af0e380d38dfac75257325ef1e8849466299ae
STEPHANIE <WU.STEPHANIE@LIVE.COM>
2017-02-08 15:01:15 -0500

    Create actions for reservations

--- app/controllers/reservations_controller.rb
+++ app/controllers/reservations_controller.rb
@@ -1,2 +1,22 @@
 class ReservationsController < ApplicationController
+
+  def show
+  end
+
+  def create
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+
+  private
+
+# this helper method maybe needs to go in applications_controller:
+  def find_reservation
+    @reservation = Reservation.find(params[:id])
+  end
+
 end
