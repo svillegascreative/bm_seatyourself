3638f82482bba1d88e80a507f59987ff1d5c0c26
STEPHANIE <WU.STEPHANIE@LIVE.COM>
2017-02-08 15:16:43 -0500

    Create restaurant validations

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -3,4 +3,8 @@ class Restaurant < ApplicationRecord
   has_many :reservations
   belongs_to :category
   # belongs_to :owner
+
+  validates :name, :address, :capacity, :phone, presence: true
+  validates :capacity, numericality: { equal_to: 100 }
+
 end
