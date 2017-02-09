1c96cf0dd5124c651640bedd22f74051c845753b
Claire Leslie <c.leslie@live.ca>
2017-02-09 15:07:47 -0500

    Create categories controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  get 'categories/index'
+
+  get 'categories/show'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :users
 
