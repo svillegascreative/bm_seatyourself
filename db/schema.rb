e53771b22f9bdb22ff1ffc8a78ab6121b771f075
Claire Leslie <c.leslie@live.ca>
2017-02-09 15:58:12 -0500

    drop categories_restaurants table

--- db/schema.rb
+++ db/schema.rb
@@ -10,17 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170209010840) do
+ActiveRecord::Schema.define(version: 20170209205612) do
 
   create_table "categories", force: :cascade do |t|
     t.string   "cuisine"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-  end
-
-  create_table "categories_restaurants", force: :cascade do |t|
-    t.integer "category_id"
-    t.integer "restaurant_id"
   end
 
   create_table "reservations", force: :cascade do |t|
