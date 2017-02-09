9f426e6f28fce2b8285a945c9b1be329c265337e
Claire Leslie <c.leslie@live.ca>
2017-02-08 20:20:55 -0500

    add owner column to user model

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,8 +1,7 @@
 class Restaurant < ApplicationRecord
-  has_many :users, through: :reservations
   has_many :reservations
   belongs_to :category
-  belongs_to :owner, class_name: "User"
+  belongs_to :user
 
   validates :name, :address, :capacity, :phone, presence: true
   validates :capacity, numericality: { equal_to: 100 }
