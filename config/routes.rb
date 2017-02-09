20496869856db76de3764d9e2462a8a6b072abb2
sylvia villegas <sylvia@svillegas.com>
2017-02-09 13:18:34 -0500

    Remove useless routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,5 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  resources :restaurants do
-    resources :reservations
-  end
-
   resources :users do
     resources :reservations, only: [:show, :update, :edit, :destroy]
     resources :restaurants do  #as owner
