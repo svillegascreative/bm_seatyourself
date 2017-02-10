5d36b754ce7de1a4383b4b53031f191f6489b8fc
STEPHANIE <WU.STEPHANIE@LIVE.COM>
2017-02-10 13:39:18 -0500

    Style images and footer

--- db/seeds.rb
+++ db/seeds.rb
@@ -12,7 +12,7 @@ oliver = User.create(:name => "Oliver", :password => "ollie", :email => "oliver@
 sarah = User.create(:name => "Sarah", :password => "couscous", :email => "sarah@gmail.com")
 
 Restaurant.create(name: "Kinka Izakaya", address: "398 Church Street", capacity: 100, phone: "416-333-4444", description: "Trendy Japanese izakaya.", website: "http://www.kinkaizakaya.com/", user_id: 1, category_id: 2, image_url: "https://s3-media4.fl.yelpcdn.com/bphoto/nHFVveLBqeiCGy12R2CnRA/o.jpg")
-Restaurant.create(name: "Boku Sushi", address: "42 Gristmill Lane", capacity: 100, phone: "416-368-8686", description: "Sushi bar located in the Distillery District.", website: "http://bokusushi.ca/", user_id: 1, category_id: 2, image_url: "https://s3-media3.fl.yelpcdn.com/bphoto/2GoGU_0e8_sRRI2jK7n45g/o.jpg")
+Restaurant.create(name: "Boku Sushi", address: "42 Gristmill Lane", capacity: 100, phone: "416-368-8686", description: "Sushi bar located in the Distillery District.", website: "http://bokusushi.ca/", user_id: 1, category_id: 2, image_url: "https://s3-media1.fl.yelpcdn.com/bphoto/jfQj8vOBXtAZ6NqS7G9WqQ/o.jpg")
 Restaurant.create(name: "Bar Hop", address: "391 King Street W.", capacity: 100, phone: "647-352-7476", description: "Largest beer selection in Toronto.", website: "http://www.barhop.com/", user_id: 2, category_id: 1, image_url: "https://s3-media2.fl.yelpcdn.com/bphoto/O0O0_yXfiATdzcmvGts12g/o.jpg")
 
 Category.create(cuisine: "Comfort Food")
