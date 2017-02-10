7a6e26d91b3b7ab55c03fd1b034f1a318e66d63f
EFFY MIN <effymin@gmail.com>
2017-02-09 19:15:29 -0500

    validations done

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -55,7 +55,6 @@ class RestaurantsController < ApplicationController
     @restaurant = Restaurant.find(params[:id])
   end
 
-
   private
   def restaurant_params
     params.require(:restaurant).permit(:name, :address, :capacity, :phone, :description, :website)
--- app/models/reservation.rb
+++ app/models/reservation.rb
@@ -3,4 +3,6 @@ class Reservation < ApplicationRecord
   belongs_to :user
   belongs_to :restaurant
 
+  validates :seats, :datetime, :year, :month, :day, :hour, presence: true
+
 end
